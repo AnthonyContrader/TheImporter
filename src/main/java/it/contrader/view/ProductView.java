4b267ba6f18f92325e439272ca992bbfd6779cd3
cala095 <48807946+cala095@users.noreply.github.com>
2020-03-17 16:17:32 +0100

    view

---
+++ src/main/java/it/contrader/view/ProductView.java
@@ -0,0 +1,68 @@
+package it.contrader.view;
+
+import java.util.List;
+
+import it.contrader.controller.Request;
+import it.contrader.dto.ProductDTO;
+import it.contrader.main.MainDispatcher;
+
+
+/**
+ * 
+ * @author Vittorio
+ *
+ * Si osservi che alla View arrivano solo oggetti di tipo DTO!
+ */
+public class ProductView extends AbstractView {
+
+	private Request request;
+	private String choice;
+
+	public ProductView() {
+		
+	}
+
+	/**
+	 * Mostra la lista utenti
+	 */
+	@Override
+	public void showResults(Request request) {
+		if (request != null) {
+			System.out.println("\n------------------- Gestione Prodotti ----------------\n");
+			System.out.println("ID\tnomeProdotto\tPrice");
+			System.out.println("----------------------------------------------------\n");
+			
+			@SuppressWarnings("unchecked")
+			List<ProductDTO> users = (List<ProductDTO>) request.get("users");
+			for (ProductDTO u: users)
+				System.out.println(u);
+			System.out.println();
+		}
+	}
+
+	/**
+	 * Chiede all'utente un input (lettera da tastiera) per la choice (vedi UserController). 
+	 * Mette la modalit� GETCHOICE nella mode.
+	 */
+	@Override
+	public void showOptions() {
+		System.out.println("          Scegli l'operazione da effettuare:");
+		System.out.println("[L]eggi [I]nserisci [M]odifica [C]ancella [B]ack [E]sci");
+
+		this.choice = getInput();
+
+		
+	}
+	
+	/**
+	 * Impacchetta la request e la manda allo UserController.
+	 */
+	@Override
+	public void submit() {
+		request = new Request();
+		request.put("choice", choice);
+		request.put("mode", "GETCHOICE");
+		MainDispatcher.getInstance().callAction("Product", "doControl", this.request);
+	}
+
+}
