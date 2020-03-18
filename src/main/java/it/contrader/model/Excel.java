d32254d052eee84abe5798494bcc582b606f996b
cala095 <48807946+cala095@users.noreply.github.com>
2020-03-18 09:57:04 +0100

    excel

---
+++ src/main/java/it/contrader/model/Excel.java
@@ -0,0 +1,24 @@
+package it.contrader.model;
+
+public class Excel {
+	
+	private String directory;
+	
+	public Excel() {}
+
+	public Excel(String directory) {
+		super();
+		this.directory = directory;
+	}
+
+	public String getDirectory() {
+		return directory;
+	}
+
+	public void setDirectory(String directory) {
+		this.directory = directory;
+	}
+	
+	
+
+}
