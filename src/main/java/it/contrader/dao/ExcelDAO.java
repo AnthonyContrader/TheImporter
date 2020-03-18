598df34e35496fd07cc43ba18305262288e7a014
cala095 <48807946+cala095@users.noreply.github.com>
2020-03-18 15:27:58 +0100

    excel

--- src/main/java/it/contrader/dao/ExcelDAO.java
+++ src/main/java/it/contrader/dao/ExcelDAO.java
@@ -5,15 +5,15 @@ import java.util.ArrayList;
 import java.util.List;
 import it.contrader.main.ConnectionSingleton;
 import it.contrader.model.Product;
-import it.contrader.model.User;
+import it.contrader.model.Excel;
 
 
 public class ExcelDAO {
 	
 	private final String QUERY_ALL = "SELECT * FROM excel";
 	private final String QUERY_CREATE = "INSERT INTO excel (productName, price) VALUES (?,?)";
-	private final String QUERY_READ = "SELECT * FROM excel WHERE id=?";
-	private final String QUERY_UPDATE = "UPDATE excel SET productName=?, price=? WHERE id=?";
+	//private final String QUERY_READ = "SELECT * FROM excel WHERE id=?";
+	//private final String QUERY_UPDATE = "UPDATE excel SET productName=?, price=? WHERE id=?";
 	private final String QUERY_DELETE = "DELETE FROM excel WHERE id=?";
 
 	public ExcelDAO() {
@@ -42,97 +42,21 @@ public class ExcelDAO {
 		return productList;
 	}
 
-	public boolean insert(Product productToInsert) {
+	public boolean insert(List<Product> productListToInsert) {
 		Connection connection = ConnectionSingleton.getInstance();
 		try {	
-			PreparedStatement preparedStatement = connection.prepareStatement(QUERY_CREATE);
-			preparedStatement.setString(1, productToInsert.getproductName());
-			preparedStatement.setInt(2, productToInsert.getprice());
-			preparedStatement.execute();
+			for(Product p: productListToInsert){
+				PreparedStatement preparedStatement = connection.prepareStatement(QUERY_CREATE);
+				preparedStatement.setString(1, p.getproductName());
+				preparedStatement.setInt(2, p.getprice());
+				preparedStatement.execute();
+			}
 			return true;
 		} catch (SQLException e) {
 			return false;
 		}
 
 	}
-	
-	public Product read(int productID) {
-		Connection connection = ConnectionSingleton.getInstance();
-		try {
-			PreparedStatement preparedStatement = connection.prepareStatement(QUERY_READ);
-			preparedStatement.setInt(1, productID);
-			ResultSet resultSet = preparedStatement.executeQuery();
-			resultSet.next();
-			String productName;
-			int price;
-
-			productName = resultSet.getString("productName");
-			price = resultSet.getInt("price");
-			
-			Product product = new Product(productID, productName, price);
-			product.setId(resultSet.getInt("id"));
-
-			return product;
-		} catch (SQLException e) {
-			return null;
-		}
-
-	}
-	
-	
-	public boolean update(Product productToUpdate) {
-		Connection connection = ConnectionSingleton.getInstance();
-
-		// Check if id is present
-		if (productToUpdate.getId() == 0)
-			return false;
-
-		Product productRead = read(productToUpdate.getId());
-		if (!productRead.equals(productToUpdate)) {
-			try {
-				// Fill the userToUpdate object
-				if (productToUpdate.getproductName() == null || productToUpdate.getproductName().equals("")) {
-					productToUpdate.setproductName(productRead.getproductName());
-				}
-
-				if (productToUpdate.getprice() == 0) {
-					productToUpdate.setprice(productRead.getprice());
-				}
-
-				// Update the user
-				PreparedStatement preparedStatement = (PreparedStatement) connection.prepareStatement(QUERY_UPDATE); 
-				preparedStatement.setString(1, productToUpdate.getproductName());
-				preparedStatement.setInt(2, productToUpdate.getprice());
-				preparedStatement.setInt(3, productToUpdate.getId());
-				int a =  preparedStatement.executeUpdate();
-				if (a > 0)
-					return true;
-				else
-					return false;
-
-			} catch (SQLException e) {
-				return false;
-			}
-		}
-
-		return false;
-
-	}
-	
-	public boolean delete(int id) {
-		Connection connection = ConnectionSingleton.getInstance();
-		try {
-			PreparedStatement preparedStatement = connection.prepareStatement(QUERY_DELETE);
-			preparedStatement.setInt(1, id);
-			int n = preparedStatement.executeUpdate();
-			if (n != 0)
-				return true;
-
-		} catch (SQLException e) {
-		}
-		return false;
-	}
-	
 
 
 }
