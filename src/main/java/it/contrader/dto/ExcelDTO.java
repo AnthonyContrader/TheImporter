988a8786712c7df1f74d29fbdcb82c388bae7e61
Gabriele-Spalletta <62241385+Gabriele-Spalletta@users.noreply.github.com>
2020-03-18 14:26:03 +0100

    g

--- src/main/java/it/contrader/dto/ExcelDTO.java
+++ src/main/java/it/contrader/dto/ExcelDTO.java
@@ -4,6 +4,10 @@ public class ExcelDTO {
 	
 	private String directory;
 	
+	private String title1;
+	
+	private String title2;
+	
 	public ExcelDTO() {	}
 	
 	
@@ -15,7 +19,22 @@ public class ExcelDTO {
 	public String getDirectory() {
 		return directory;
 	}
+	
+	public String getTitle1() {
+		return title1;
+	}
+	
+	public String getTitle2() {
+		return title2;
+	}
 
+	public void setTitle1(String title1) {
+		this.title1=title1;
+	}
+	
+	public void setTitle2(String title2) {
+		this.title2=title2;
+	}
 	public void setDirectory(String directory) {
 		this.directory = directory;
 	}
