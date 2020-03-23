139abf0ce7bfb2038a3e360ae48899e251186bca
Gabriele-Spalletta <62241385+Gabriele-Spalletta@users.noreply.github.com>
2020-03-23 10:44:44 +0100

    Added HistoryService

---
+++ src/main/java/it/contrader/service/HistoryService.java
@@ -0,0 +1,28 @@
+package it.contrader.service;
+
+import java.util.List;
+
+import it.contrader.converter.HistoryConverter;
+import it.contrader.dao.HistoryDAO;
+import it.contrader.dto.HistoryDTO;
+import it.contrader.model.Product;
+
+public class HistoryService {
+	private HistoryDAO historyDAO;
+	private HistoryConverter historyConverter;
+	
+	public HistoryService(){
+		this.historyDAO = new HistoryDAO();
+		this.historyConverter = new HistoryConverter();
+	}
+	
+	public boolean insert(HistoryDTO dto) {
+		// Converte un DTO in entit� e lo passa al DAO per l'inserimento
+		//return ExcelDAO.insert(ExcelConverter.toEntity(dto).getProductsList());
+		return historyDAO.insert(historyConverter.toEntity(dto));
+	}
+	
+	public List<Product> serchByUserId(HistoryDTO dto) {
+		return historyDAO.searchByUserId(historyConverter.toEntity(dto).getIdUser());
+	}
+}
