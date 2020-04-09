65f72000accfc39d256967c94da3dbe6ef483e85
cala095 <48807946+cala095@users.noreply.github.com>
2020-04-09 10:11:13 +0200

    up

---
+++ src/service/excel.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { AbstractService } from './abstractservice';
+import { ExcelDTO } from 'src/dto/exceldto';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExcelService extends AbstractService<ExcelDTO>{
+
+  constructor(http: HttpClient) {
+	super(http);
+    this.type = 'excel';
+ }
+	
+	type: string;
+    port: string = '8080';
+
+	preinsert(directory: string): Observable<any> {
+        return this.http.post('http://localhost:' + this.port + '/' + this.type + '/preinsert', directory);
+    }
+
+}
--- src/service/product.service.ts
+++ src/service/product.service.ts
@@ -4,6 +4,7 @@ import { ProductDTO } from 'src/dto/productdto';
 import { HttpClient } from '@angular/common/http';
 
 
+
 /**
  * I service sono decorati da @Injectable. 
  * Qui trovate, oltre ai metodi ereditati dall'Abstract,
