aa98c0376369b141e63be7c4c5ef9b428c14f53a
Gabriele-Spalletta <62241385+Gabriele-Spalletta@users.noreply.github.com>
2020-04-20 10:41:04 +0200

    final sign-up

--- src/app/login/sing-up/sing-up.component.ts
+++ src/app/login/sing-up/sing-up.component.ts
@@ -17,16 +17,18 @@ export class SingUpComponent implements OnInit {
   lenght: string = "";
   email: string = "";
   validuser: string = "";
-  check: boolean;
 
   user: UserDTO;
   usertoinsert: ManagedUserVM;
-  prova: string="";
+  prova: string = "";
 
   constructor(private service: UserService) { }
 
   ngOnInit() {
     //this.service.login(null);
+    this.checkEmail();
+    this.checkLenght();
+    this.checkUser();
   }
 
 
@@ -34,46 +36,64 @@ export class SingUpComponent implements OnInit {
     var a = (<HTMLInputElement>document.getElementById("pass1")).value;
     var b = (<HTMLInputElement>document.getElementById("pass2")).value;
     if (a == b) {
-      this.checked = "uguali";
+      this.checked = ":uguali";
       return true;
     } else {
-      this.checked = "diverse";
+      this.checked = ":diverse";
       return false;
     }
   }
   checkLenght(): boolean {
     var a = (<HTMLInputElement>document.getElementById("pass1")).value;
-    if (a.length >= 8) {
-      this.lenght = "";
-      return true;
+    if (a != "") {
+      if (a.length >= 8) {
+        this.lenght = "";
+        return true;
+      } else {
+        this.lenght = ":password corta";
+        return false;
+      }
     } else {
-      this.lenght = "password corta";
-      return false;
+      this.lenght = ":campo richiesto";
     }
   }
   checkEmail(): boolean {
     var a = (<HTMLInputElement>document.getElementById("e-mail")).value;
-    if (a.indexOf("@") > 1 && a.indexOf(".") > 3) {
-      this.email = "";
-      return true;
-    }
-    else {
-      this.email = "e-mail non corretta";
+    if (a != "") {
+      if (a.indexOf("@") > 1 && a.indexOf(".") > 3) {
+        this.email = "";
+        return true;
+      }
+      else {
+        this.email = ":e-mail non corretta";
+        return false;
+      }
+    } else {
+      this.email = ":campo richiesto";
       return false;
     }
 
   }
-  checkUser(): void {
+  checkUser(): boolean {
+    var a = (<HTMLInputElement>document.getElementById("user")).value;
+    if (a != "") {
+      return true;
+    } else {
+      this.validuser = ":campo richiesto";
+      return false;
+    }
   }
 
   singup(): void {
-    if(this.checkEmail() && this.checkLenght()&& this.checkPass()){
-      this.usertoinsert=new ManagedUserVM();
-      this.usertoinsert.email=(<HTMLInputElement>document.getElementById("e-mail")).value;
-      this.usertoinsert.login=(<HTMLInputElement>document.getElementById("user")).value;
-      this.usertoinsert.password=(<HTMLInputElement>document.getElementById("pass1")).value;
-      this.usertoinsert.authorities[0]="ROLE_USER";
-      this.service.signup(this.usertoinsert).subscribe((token:any) =>{console.log(token)});
-    }  
+    if (this.checkEmail() && this.checkLenght() && this.checkPass() && this.checkUser()) {
+      this.usertoinsert = new ManagedUserVM();
+      this.usertoinsert.email = (<HTMLInputElement>document.getElementById("e-mail")).value;
+      this.usertoinsert.login = (<HTMLInputElement>document.getElementById("user")).value;
+      this.usertoinsert.password = (<HTMLInputElement>document.getElementById("pass1")).value;
+      this.usertoinsert.authorities[0] = "ROLE_USER";
+      this.service.signup(this.usertoinsert).subscribe((token: any) => { console.log(token) });
+    } else {
+      alert("campi non inseriti o errati");
+    }
   }
 }
